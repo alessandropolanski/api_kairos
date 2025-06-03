4002cede5fc5b01276065d172808441a66108ff5
alessandropolanski <alessandropolanski91@gmail.com>
2025-06-03 08:55:54 -0300

    Definição de Schema do User

--- database/database.ts
+++ database/database.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
-// Configuração da conexão com MongoDB Atlas
-const uri = process.env.MONGODB_URI || 'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<database>?retryWrites=true&w=majority';
+// Configuração da conexão com MongoDB Docker
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/kairos';
 
 const client = new MongoClient(uri);
 const db = client.db('kairos');
--- models/User.ts
+++ models/User.ts
@@ -1,28 +1,27 @@
-import { NOMEM } from "dns";
-import { Schema, model } from "mongoose";
+import { Schema, model, Document } from "mongoose";
 
 export interface IUser extends Document {
+    pki: string;
+    name: string;
     email: string;
-    name?: string;
-    avatar?: string;
-  }
-  
-  const userSchema = new Schema<IUser>({
-    email: { type: String, required: true, unique: true },
-    name: String,
-    avatar: String,
-  });
-  
-  export const User = model<IUser>('User', userSchema);
+    password: string;
+    active: boolean;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+    lastModifiedBy: string;
+}
 
-// Precisa atualizar esse schema para refletir os dados necessários
-// id 
-// pki 
-// name 
-// email
-// password
-// active
-// role
-// createdAt
-// updatedAt
-// lastModifiedBy
+const userSchema = new Schema<IUser>({
+    pki: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    active: { type: Boolean, default: true },
+    role: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    lastModifiedBy: { type: String, required: true }
+});
+
+export const User = model<IUser>('User', userSchema);
