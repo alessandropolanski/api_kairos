76df46764ea1b30d32431b0ad4567addc76c3cac
alessandropolanski <alessandropolanski91@gmail.com>
2025-06-05 16:41:22 -0300

    Teste

--- database/database.ts
+++ database/database.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 // Configuração da conexão com MongoDB Docker
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/kairos';
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27018/kairos';
 
 const client = new MongoClient(uri);
 const db = client.db('kairos');
